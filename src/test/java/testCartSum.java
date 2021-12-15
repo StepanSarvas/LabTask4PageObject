3d17187aefb7995f3c0d5257a821a09c479a18a1
StepanSarvas <sarvassr@gmail.com>
2021-12-15 20:57:22 +0200

    minor edits

--- src/test/java/testCartSum.java
+++ src/test/java/testCartSum.java
@@ -4,26 +4,27 @@ import parsers.jaxb.XMLParser;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class testCartSum extends DefaultTest{
-    String keywordFromXML = parsers.jaxb.XMLParser.unmarshall().getKeyword();
-    String brandNameFromXML = parsers.jaxb.XMLParser.unmarshall().getBrandName();
-    String priceValidationFromXML = XMLParser.unmarshall().getPriceValidation();
+    private static final int TIME_TO_WAIT = 15;
+    private static String keywordFromXML = parsers.jaxb.XMLParser.unmarshall().getKeyword();
+    private static String brandNameFromXML = parsers.jaxb.XMLParser.unmarshall().getBrandName();
+    private static String priceValidationFromXML = XMLParser.unmarshall().getPriceValidation();
 
     @Test
     public void checkPriceOfItem(){
         getMainPage().searchByCategory(keywordFromXML);
         getSearchResultPage().scrollToSidebarSearch();
-        getSearchResultPage().loadElementWaiter(10, getSearchResultPage().getSidebarSearch());
+        getSearchResultPage().loadElementWaiter(TIME_TO_WAIT, getSearchResultPage().getSidebarSearch());
         getSearchResultPage().searchByBrand(brandNameFromXML);
-        getSearchResultPage().loadElementWaiter(10, getSearchResultPage().getBrandCheckbox());
+        getSearchResultPage().loadElementWaiter(TIME_TO_WAIT, getSearchResultPage().getBrandCheckbox());
         getSearchResultPage().clickOnBrandCheckbox();
         getSortedPage().sortByPriceDecrease();
-        getSortedPage().loadPageWaiter(20);
+        getSortedPage().loadPageWaiter(TIME_TO_WAIT);
         getSortedPage().clickOnFirstElement();
-        getProductPage().loadElementWaiter(10, getProductPage().getProductDescription());
+        getProductPage().loadElementWaiter(TIME_TO_WAIT, getProductPage().getProductDescription());
         getProductPage().clickOnProductDescription();
-        getProductPage().loadElementWaiter(10, getProductPage().getBuyButton());
+        getProductPage().loadElementWaiter(TIME_TO_WAIT, getProductPage().getBuyButton());
         getProductPage().clickOnBuyButton();
-        getProductPage().loadElementWaiter(10, getProductPage().getCartSum());
+        getProductPage().loadElementWaiter(TIME_TO_WAIT, getProductPage().getCartSum());
         assertEquals(getProductPage().getCartSumValue(), Integer.parseInt(priceValidationFromXML));
     }
 
